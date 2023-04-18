ab2385ab316417fa3cc53ec3f1a628d4014a52de
Gaoyuan Zhang <zgy25483387@gmail.com>
2023-04-18 11:04:30 -0700

    W4D2 Delete finished

--- datastore/index.js
+++ datastore/index.js
@@ -51,33 +51,67 @@ exports.readAll = (callback) => {
 };
 
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
+  // Read file from dataDir based on id
+  fs.readFile(`${exports.dataDir}/${id}.txt`, function(err, data) {
+    if (!data) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      callback(null, { id: id, text: data.toString() });
+    }
+  });
+  // Construct a object that has id and text
+  // var text = items[id];
+  // if (!text) {
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback(null, { id: id, text: data });
+  // }
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  // Read file from dataDir based on id
+  fs.readFile(`${exports.dataDir}/${id}.txt`, function(err, data) {
+    if (!data) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      // Update the text in the file and Overwrite the file
+      fs.writeFile(`${exports.dataDir}/${id}.txt`, text, function() {
+        // items[id] = text;
+        callback(null, { id: id, text: text });
+      });
+    }
+  });
+  // var item = items[id];
+  // if (!item) {
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   items[id] = text;
+  //   callback(null, { id, text });
+  // }
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  // Read file from dataDir based on id
+  fs.readFile(`${exports.dataDir}/${id}.txt`, function(err, data) {
+    if (!data) {
+      // report an error if item not found
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      // Delete file from dataDir based on id
+      fs.unlink(`${exports.dataDir}/${id}.txt`, function() {
+        callback();
+      });
+    }
+  });
+  // Delete file from dataDir based on id
+  // var item = items[id];
+  // delete items[id];
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  // }
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
